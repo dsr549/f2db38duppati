c65eee54392973da198e07dd3ea7b413e9fcfd27
sudheer duppati <S555338@nwmissouri.edu>
2022-11-18 00:46:50 -0600

    added

--- routes/squirrel.js
+++ routes/squirrel.js
@@ -6,13 +6,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('squirrel', { title: 'Search Results squirrel' });
 });
-module.exports = router;
+
 var express = require('express');
 const squirrel_controlers= require('../controllers/squirrel');
 var router = express.Router();
 /* GET squirrels */
+router.get('/', squirrel_controlers.squirrel_view_all_Page );
+/* GET detail squirrel page */
 router.get('/detail', squirrel_controlers.squirrel_view_one_Page);
-//router.get('/', squirrel_controlers.squirrel_view_all_Page );
 /* GET create squirrel page */
 router.get('/create', squirrel_controlers.squirrel_create_Page);
 /* GET create update page */
@@ -20,4 +21,4 @@ router.get('/update', squirrel_controlers.squirrel_update_Page);
 /* GET delete squirrel page */
 router.get('/delete', squirrel_controlers.squirrel_delete_Page);
 module.exports = router;
-/* GET detail squirrel page */
+
